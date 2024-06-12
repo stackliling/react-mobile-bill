7a17f9e716268fa7b4e5925e3c67b5a30bb82517
liling02 <lingli02@wisedu.com>
2024-06-12 17:44:41 +0800

    feat: 月度账单查询，统计

--- src/pages/MonthBill/index.jsx
+++ src/pages/MonthBill/index.jsx
@@ -1,18 +1,73 @@
 import { NavBar, DatePicker, Toast } from "antd-mobile";
-import { DownOutline } from "antd-mobile-icons";
+import { DownOutline, UpOutline } from "antd-mobile-icons";
 import "./index.css";
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
+import dayjs from "dayjs";
+import { useSelector } from "react-redux";
+import _ from "lodash";
 
 export default function MonthBill() {
+  const [visible, setVisible] = useState(false);
 
-  const [visible, setVisible] = useState(true)
+  const [currDate, setCurrDate] = useState(dayjs(new Date()).format("YYYY-MM"));
+
+  const { billList } = useSelector((state) => state.bill);
+
+
+  const currentCost = useMemo(() => {
+    // 对账单数据进行分组，以月份为分组条件
+    // const group = _.groupBy(billList,(item) => dayjs(item.date).format('YYYY/MM'))
+    // console.log(group);
+
+    console.log(billList);
+
+    const thisMonth = dayjs(currDate).format("YYYY/MM");
+    console.log(thisMonth);
+
+    const incomeList = _.groupBy(
+      billList.filter((a) => !a.isPay),
+      (item) => dayjs(item.date).format("YYYY/MM")
+    )[thisMonth];
+
+    console.log(incomeList);
+
+    const payList = _.groupBy(
+      billList.filter((a) => a.isPay),
+      (item) => dayjs(item.date).format("YYYY/MM")
+    )[thisMonth];
+
+    let income,
+      pay = 0;
+
+    if (incomeList) {
+      income = incomeList.reduce((a, c) => {
+        return a + c.amount;
+      }, 0);
+    }
+
+    if (payList) {
+      pay = payList.reduce((a, c) => {
+        return a + c.amount;
+      }, 0);
+    }
+
+    console.log(income);
+
+    return {
+      income: income,
+      pay: pay,
+      total: income + pay,
+    };
+  }, [billList, currDate]);
+
 
   const handleClickArrow = () => {
-    console.log(1);
-    setVisible((state) => {
-      state.visible = !state.visible
-    })
-  }
+    setVisible(true);
+  };
+
+  const onConfirm = (val) => {
+    setCurrDate(dayjs(val).format("YYYY-MM"));
+  };
 
   return (
     <>
@@ -22,23 +77,24 @@ export default function MonthBill() {
           <div className="header">
             {/* 时间切换区域 */}
             <div className="date">
-              <span className="text">2023 | 3月账单</span>
-              <span className="arrow" onClick={() => handleClickArrow}>
-                <DownOutline />
+              <span className="text">{currDate} 月账单</span>
+              <span className="arrow" onClick={handleClickArrow}>
+                {!visible && <DownOutline />}
+                {visible && <UpOutline />}
               </span>
             </div>
             {/* 统计区域 */}
             <div className="overview">
               <div className="item">
-                <span className="money">(100)</span>
+                <span className="money">({currentCost.pay})</span>
                 <span className="type">支出</span>
               </div>
               <div className="item">
-                <span className="money">(200)</span>
+                <span className="money">({currentCost.income})</span>
                 <span className="type">收入</span>
               </div>
               <div className="item">
-                <span className="money">(200)</span>
+                <span className="money">({currentCost.total})</span>
                 <span className="type">结余</span>
               </div>
             </div>
@@ -48,12 +104,10 @@ export default function MonthBill() {
         <DatePicker
           visible={visible}
           onClose={() => {
-            setVisible(false)
+            setVisible(false);
           }}
-          precision='month'
-          onConfirm={val => {
-            Toast.show(val.toString())
-          }}
+          precision="month"
+          onConfirm={(val) => onConfirm(val)}
         />
       </div>
     </>
